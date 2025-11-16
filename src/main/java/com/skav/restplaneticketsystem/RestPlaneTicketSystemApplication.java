f7bd0d9cf2fd086bf46e57e71fbdeaf4e54be97d
Dawid Smulkowski <dawidsmulkowski@gmail.com>
2025-11-16 17:29:12 +0100

    Init project

---
+++ src/main/java/com/skav/restplaneticketsystem/RestPlaneTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.skav.restplaneticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaneTicketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPlaneTicketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skav/restplaneticketsystem/RestPlaneTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skav.restplaneticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPlaneTicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
